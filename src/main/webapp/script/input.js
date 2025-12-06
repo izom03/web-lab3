4aca8373f143d83f23478c79525c52c5b0d7f474
iizqm@archlinux <izomanfan@gmail.com>
2025-12-06 19:48:26 +0300

    Fixed input text

--- src/main/webapp/script/input.js
+++ src/main/webapp/script/input.js
@@ -3,7 +3,7 @@ function allowOnlyNumber(e) {
     const v = e.target.value;
     if (k.length > 1) return true;
     if (k >= '0' && k <= '9') return true;
-    if (k === '.' && !v.includes('.') && v.length > 0) return true;
+    if (k === '.' && !v.includes('.') && v.length > 0 && v[v.length - 1] !== "-") return true;
     if (k === '-' && v.length === 0 && !v.includes('-')) return true;
     e.preventDefault();
     return false;
